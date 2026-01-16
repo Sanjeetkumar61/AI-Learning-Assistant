44d74bffc806c5c71991a2793ffc29c335021511
Sanjeet Kumar <156106398+Sanjeetkumar61@users.noreply.github.com>
2026-01-16 14:07:31 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ connectDB();
 // Middleware to handle CORS
 app.use(
   cors({
-    origin: "*",
+    origin: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
